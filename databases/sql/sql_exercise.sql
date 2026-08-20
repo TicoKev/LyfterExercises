ff5d1c00a45aaa13ca11a7882773e6f0f4c73baa
Kevin Mora Chavarria <kmora9871@gmail.com>
2026-08-19 20:57:52 -0600

    (fix): add INSERT commands

--- databases/sql/sql_exercise.sql
+++ databases/sql/sql_exercise.sql
@@ -84,6 +84,74 @@ ALTER TABLE invoices
   ADD employee_code CHAR(6) DEFAULT 'ABC123' NOT NULL 
 
 
+-- Commands to add items to the tables
+
+INSERT INTO users (full_name, email, registration_date)
+  VALUES 
+    ('John Smith', 'john.smith@example.com', '2026-08-01'),
+    ('Emily Johnson', 'emily.johnson@example.com', '2026-08-05'),
+    ('Michael Brown', 'michael.brown@example.com', '2026-08-10');
+
+INSERT INTO products (code, name, price, entry_date, brand, stock_available)
+  VALUES
+    ('PRD1001', 'Gaming Laptop', 1200.50, '2026-08-01', 'Asus', 10),
+    ('PRD1002', 'Wireless Mouse', 25.99, '2026-08-02', 'Logitech', 50),
+    ('PRD1003', 'Mechanical Keyboard', 75.00, '2026-08-03', 'Corsair', 30),
+    ('PRD1004', '4K OLED TV', 5200.00, '2026-08-04', 'Samsung', 5),
+    ('PRD1005', 'Luxury Smartwatch', 6500.00, '2026-08-05', 'Apple', 8),
+    ('PRD1006', 'Professional Camera', 7200.00, '2026-08-06', 'Canon', 4);
+
+INSERT INTO invoices (invoice_number, purchase_date, total_amount, buyer_phone_number, employee_code)
+  VALUES
+    (2001, '2026-08-10', 8644.46, '+7872361219', 'EMP001'),
+    (2002, '2026-08-12', 16727.48, '+9613214731', 'EMP002'),
+    (2003, '2026-08-15', 20925.50, '+7875556331', 'EMP001'),
+    (2004, '2026-08-18', 7200.00, '+50688887777', 'EMP003'),
+    (2005, '2026-08-19', 7450.50, '+50699996666', 'EMP002');
+
+INSERT INTO shopping_cart (buyer_email)
+  VALUES
+    ('john.smith@example.com'),
+    ('emily.johnson@example.com');
+
+INSERT INTO shopping_cart_products (shopping_cart_id, product_id)
+  VALUES
+  (1, 3),   
+  (1, 6),   
+  (2, 4);
+
+INSERT INTO reviews (product_code, comment, rating, date)
+  VALUES
+    ('PRD001', 'Excellent performance, very fast.', 5, '2026-08-12'),
+    ('PRD002', 'Comfortable but the battery does not last long.', 3, '2026-08-14'),
+    ('PRD003', 'Keyboard with good sound and quality.', 4, '2026-08-16');
+
+INSERT INTO payment_method (method_type, bank_name)
+  VALUES
+    ('Credit Card', 'Bank of America'),
+    ('PayPal', ''),
+    ('Wire Transfer', 'Chase Bank');
+
+INSERT INTO products_per_invoice (product_id, invoice_id, quantity, total_amount)
+  VALUES
+    (1, 1, 1, 1200.50),
+    (2, 1, 1, 25.99),
+    (3, 1, 2, 150.00),
+    (2, 1, 3, 77.97),
+    (6, 1, 1, 7200.00),
+    (4, 2, 1, 5200.00),
+    (4, 2, 2, 10400.00),
+    (1, 2, 1, 1200.50),
+    (3, 2, 1, 75.00),
+    (2, 2, 2, 51.98),
+    (5, 3, 1, 6500.00),
+    (5, 3, 2, 13000.00),
+    (1, 3, 1, 1200.50),
+    (3, 3, 3, 225.00),
+    (6, 4, 1, 7200.00),
+    (1, 5, 1, 1200.50),
+    (6, 5, 1, 6250.00);
+
 -- 3. SELECT Exercises
 
 -- 1. Get all stored products
